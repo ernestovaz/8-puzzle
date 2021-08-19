68df51fd7f01d9ebd7372e50139faa6058928cdf
ernesto <ernesto.oliveira@inf.ufrgs.br>
2021-08-19 18:33:44 -0300

    criado arquivo e função de mover peças

---
+++ solucao.py
@@ -0,0 +1,19 @@
+ESQUERDA = -1
+DIREITA = 1
+ABAIXO = 3
+ACIMA = -3
+
+#função que move peça, dado estado inicial, índice do espaço vazio e direção
+def moveEstado(estado: str, idxVazio: int, dir: int):
+    proxEstado = []
+    idxProx = idxVazio + dir
+    for idx,n in enumerate(estado):
+        if idx == idxVazio:
+            proxEstado.append(estado[idxProx])
+        elif idx == idxProx:
+            proxEstado.append("_")
+        else:
+            proxEstado.append(n)
+    return "".join(proxEstado)
+
+
